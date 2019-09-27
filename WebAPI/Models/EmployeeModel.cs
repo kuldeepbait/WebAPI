98180a19267822525c9d754e2c59164cbf64660c
Kuldeep <Kuldeepbait@gmail.com>
2019-09-28 00:52:58 +0530

    added important project

---
+++ WebAPI/Models/EmployeeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI.DLL;
+
+namespace WebAPI.Models
+{
+    public class EmployeeModel
+    {
+       
+
+        public int id { get; set; }
+
+       
+        public string name { get; set; }
+
+
+        
+        public decimal Salary { get; set; }
+
+         
+    }
+
+   
+}
---
+++ WebAPI/api/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebAPI.DLL;
+using WebAPI.Models;
+
+namespace WebAPI.api
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EmployeeController : ApiController
+    {
+
+        TestEntities db = new TestEntities();
+        [Route("api/GetEmployeeList")]
+        public IHttpActionResult GetEmployeeList()
+        {
+            return Json(db.Employees.ToList());
+        }
+        [Route("api/GetCityList")]
+        public IHttpActionResult GetCityList()
+        {
+            return Json(db.Cities.ToList());
+        }
+        [Route("api/SaveEmployee")]
+        [HttpPost]
+        public IHttpActionResult SaveEmployee([FromBody]EmployeeModel emp)
+        {
+            return Json("success");
+        }
+    }
+}
